71b2bd8975241d9fd8d8d49291621838a403a870
sjok <sjok101@gmail.com>
2024-02-16 19:42:55 -0500

    port 8080

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/", function (req, res) {
 //--------------------------------------//
 
 // Start the server to listen on port 3000
-const port = 80;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
